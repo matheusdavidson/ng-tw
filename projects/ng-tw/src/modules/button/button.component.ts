3d941015f5bc96b572cb29023e5f549f3dedad8f
Matheus Davidson <matheusdavidson@gmail.com>
2022-01-28 02:12:37 -0300

    feat(button): initial button component

---
+++ projects/ng-tw/src/modules/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tw-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
