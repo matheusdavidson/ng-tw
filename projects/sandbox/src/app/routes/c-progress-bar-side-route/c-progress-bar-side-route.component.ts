583f9bb1df0e436b94fba5a1920497b659d43ca0
Matheus Davidson <matheusdavidson@gmail.com>
2022-04-29 00:44:26 -0300

    feat(progress-bar): side navigation

--- projects/sandbox/src/app/routes/c-progress-bar-side-route/c-progress-bar-side-route.component.ts
+++ projects/sandbox/src/app/routes/c-progress-bar-side-route/c-progress-bar-side-route.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-c-progress-bar-side-route',
-  templateUrl: './c-progress-bar-side-route.component.html',
-  styleUrls: ['./c-progress-bar-side-route.component.scss']
+    selector: 'app-c-progress-bar-side-route',
+    templateUrl: './c-progress-bar-side-route.component.html',
+    styleUrls: ['./c-progress-bar-side-route.component.scss'],
 })
 export class CProgressBarSideRouteComponent implements OnInit {
+    public navigation: any[] = [
+        {
+            label: 'Progress Bar',
+            alt: '',
+            fragment: 'progress-bar',
+            link: '/components/progress-bar',
+        },
+        {
+            label: 'Basic Usage',
+            alt: '',
+            fragment: 'basic-usage',
+            link: '/components/progress-bar',
+        },
+    ];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
