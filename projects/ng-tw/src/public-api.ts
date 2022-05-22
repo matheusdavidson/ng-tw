aa11e3ad02b64f5834a108784988677ae9cc1fa8
Matheus Davidson <matheusdavidson@gmail.com>
2022-05-21 22:18:27 -0300

    feat(module/config)

--- projects/ng-tw/src/public-api.ts
+++ projects/ng-tw/src/public-api.ts
@@ -4,9 +4,13 @@
 
 export * from './modules/button/button.component';
 export * from './modules/button/button.module';
+export * from './modules/button/button-config.interface';
+export * from './modules/button/button-config.service';
 
 export * from './modules/select/select.component';
 export * from './modules/select/select.module';
+export * from './modules/select/select-config.interface';
+export * from './modules/select/select-config.service';
 
 export * from './modules/option/option.component';
 export * from './modules/option/option.module';
@@ -15,7 +19,6 @@ export * from './modules/notification/notification-config';
 export * from './modules/notification/notification-ref';
 export * from './modules/notification/notification.component';
 export * from './modules/notification/notification.module';
-export * from './modules/notification/notification-config.module';
 export * from './modules/notification/notification.service';
 
 export * from './modules/progress-bar/progress-bar.component';
@@ -26,3 +29,5 @@ export * from './modules/dropdown/dropdown-trigger-for.directive';
 export * from './modules/dropdown/dropdown-item.component';
 export * from './modules/dropdown/dropdown.component';
 export * from './modules/dropdown/dropdown.module';
+export * from './modules/dropdown/dropdown-config.interface';
+export * from './modules/dropdown/dropdown-config.service';
