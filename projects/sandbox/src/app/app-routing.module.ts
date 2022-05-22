97e4d5df43309b7af9b130fa0ceb53e1adc70638
Matheus Davidson <matheusdavidson@gmail.com>
2022-05-22 02:26:32 -0300

    chore(doc): seo

--- projects/sandbox/src/app/app-routing.module.ts
+++ projects/sandbox/src/app/app-routing.module.ts
@@ -19,11 +19,11 @@ const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {
-            scrollPositionRestoration: 'enabled',
-            anchorScrolling: 'enabled',
-            scrollOffset: [0, 84],
-            initialNavigation: 'enabledBlocking',
-        }),
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    scrollOffset: [0, 84],
+    initialNavigation: 'enabledBlocking',
+}),
     ],
     exports: [RouterModule],
 })
