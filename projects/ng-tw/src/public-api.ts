84f364ce209b9ae7cee35a0b4f55d2735f2a13af
Finn <81511282+finngreig@users.noreply.github.com>
2023-09-15 17:38:32 +1200

    Added missing interface and service exports

--- projects/ng-tw/src/public-api.ts
+++ projects/ng-tw/src/public-api.ts
@@ -34,6 +34,10 @@ export * from './modules/dropdown/dropdown-config.service';
 
 export * from './modules/checkbox/checkbox.component';
 export * from './modules/checkbox/checkbox.module';
+export * from './modules/checkbox/checkbox-config.interface';
+export * from './modules/checkbox/checkbox-config.service';
 
 export * from './modules/toggle/toggle.component';
 export * from './modules/toggle/toggle.module';
+export * from './modules/toggle/toggle-config.interface';
+export * from './modules/toggle/toggle-config.service';
